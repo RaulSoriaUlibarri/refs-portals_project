2bc34961f2f07f871b6b144bf81fe66cfa6afa5f
Ruls <soria.developer@gmail.com>
2026-05-17 04:55:20 -0600

    feat: add changeName functionality with useref

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState(null);
+
+  const newName = useRef();
+
+  function handleClick() {
+    setPlayerName(newName.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={newName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
