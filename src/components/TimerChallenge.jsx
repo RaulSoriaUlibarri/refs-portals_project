56c6948803a202615d298e4cfb41398a9bc70f3e
Ruls <soria.developer@gmail.com>
2026-05-18 15:57:06 -0600

    feature(components): add basic timerchallenge structure

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,16 @@
+const TimerChallenge = ({ title, targetTime }) => {
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      <p className="challenge-time">
+        {targetTime} second{title > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button>Start Challenge</button>
+      </p>
+      <p className="">Time is running... / Time inactive</p>
+    </section>
+  );
+};
+
+export default TimerChallenge;
