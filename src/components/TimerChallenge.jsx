f7497a7fe68fadfcc1c5ba90f0e7a0134c9bf72e
Ruls <soria.developer@gmail.com>
2026-05-18 19:40:38 -0600

    feature(timerchallenge): integrate resultmodal component

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,13 +1,16 @@
 import { useState, useRef } from "react";
+import ResultModal from "./ResultModal";
 
 const TimerChallenge = ({ title, targetTime }) => {
   const [timeStarted, setTimeStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
   const timer = useRef();
+  const dialog = useRef();
 
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.showModal();
     }, targetTime * 1000);
 
     setTimeStarted(true);
@@ -18,21 +21,24 @@ const TimerChallenge = ({ title, targetTime }) => {
   }
 
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      {timerExpired && <p>'You Lost'</p>}
-      <p className="challenge-time">
-        {targetTime} second{title > 1 ? "s" : ""}
-      </p>
-      <p>
-        <button onClick={timeStarted ? handleStop : handleStart}>
-          {timeStarted ? "Stop" : "Start"} Challenge
-        </button>
-      </p>
-      <p className={timeStarted ? "active" : undefined}>
-        {timeStarted ? "Time is running..." : "Time inactive"}
-      </p>
-    </section>
+    <>
+      <ResultModal ref={dialog} result="lost" targetTime={targetTime} />
+
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second{title > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timeStarted ? handleStop : handleStart}>
+            {timeStarted ? "Stop" : "Start"} Challenge
+          </button>
+        </p>
+        <p className={timeStarted ? "active" : undefined}>
+          {timeStarted ? "Time is running..." : "Time inactive"}
+        </p>
+      </section>
+    </>
   );
 };
 
