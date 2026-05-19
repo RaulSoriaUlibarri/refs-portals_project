dc4e99e2d2ef1f821990e5cb0480e2e228f413f8
Ruls <soria.developer@gmail.com>
2026-05-18 19:39:39 -0600

    feature(components): add resultmodal component basic structure

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,18 @@
+const ResultModal = ({ ref, result, targetTime }) => {
+  return (
+    <dialog ref={ref} className="result-modal">
+      <h2>Your result: {result}</h2>
+      <p>
+        The target time was <strong>{targetTime} seconds</strong>
+      </p>
+      <p>
+        You stop the timer with <strong>X seconds left</strong>
+      </p>
+      <form method="dialog">
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+};
+
+export default ResultModal;
